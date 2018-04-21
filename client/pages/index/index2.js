b3b26426fc18988b6c3490b8e290c8f0c03f19a5
zhaozichuan <zhaozichuan@sina.com>
2018-04-21 22:28:05 +0800

    aa

---
+++ client/pages/index/index2.js
@@ -0,0 +1,84 @@
+// pages/index/index2.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    result:{}
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this;
+    
+    wx.request({
+      url: 'http://zhaozichuan.iask.in/myStockList?mid=1&userId=1', //仅为示例，并非真实的接口地址
+      
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        console.log(res.data)
+        that.setData(
+          {result:res.data}
+        );
+      }
+    })
+
+
+
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
